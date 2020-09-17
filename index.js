4012bad02d7fa49c2f4bf5cdc44c45b71ecd1de5
Pebanh <peter.banh95@gmail.com>
2020-09-17 14:28:34 -0400

    Troubleshoot highlightjs

--- index.js
+++ index.js
@@ -4,16 +4,28 @@ const previewArea = document.querySelector('#preview');
 let result = '';
 
 // Call MarkdownIT
-const md = window.markdownit();
+// Modify this to accept the highlight function so MarkdownIt knows how to highlight my code.
+const md = window.markdownit({
+  highlight: function (str, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return hljs.highlight(lang, str).value;
+      } catch (__) { }
+    }
+
+    return ''; // use external default escaping
+  }
+})
 hljs.initHighlightingOnLoad();
 // console.log(md);
 // console.log(hljs);
 
 function handleChange() {
   // Retrieve markdown and render html
-  let text = markdownArea.textContent;
+  let text = markdownArea.value;
   result = md.render(text);
-
+  console.log(text)
+  console.log(result)
   // Send converted markdown to the Preview
   previewArea.innerHTML = result;
 }
